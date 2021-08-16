d90ce6e6f304c4a0c81d64a399fbf335efda15ee
freaklucas <lucas.oliveira.freak@gmail.com>
2021-08-16 19:31:18 -0300

    email e name nos inputs

--- frontend/src/components/admin/UserAdmin.vue
+++ frontend/src/components/admin/UserAdmin.vue
@@ -1,41 +1,79 @@
 <template>
   <div class="container">
     <div class="user-admin flex justify-center content-center">
-      <div class="flex flex-col">
+      <div class="flex flex">
         <form class="w-full max-w-lg">
           <div class="flex flex-wrap -mx-3 mb-6">
             <label
               class="
-                block
-                uppercase
-                tracking-wide
-                text-gray-700 text-xs
-                font-bold
-                mb-2
+              block
+              uppercase
+              tracking-wide
+              text-gray-700 text-xs
+              font-bold
+              mb-2
               "
               for="grid-first-name"
-            >
+              >
               Nome:
             </label>
             <input
               class="
-                appearance-none
-                bg-transparent
-                border-none
-                w-full
-                text-gray-700
-                mr-3
-                py-1
-                px-2
-                mb-4
-                mt-6
-                leading-tight
-                focus:outline-none
-              "
+              appearance-none
+              bg-transparent
+              border-none
+              w-full
+              text-gray-700
+              text-xl
+              hover:bg-gray-200
+              rounded-full
+              mr-3
+              py-1
+              px-2
+              mb-2
+              mt-2
+              leading-tight
+              focus:outline-none"
               type="text"
               placeholder="Informe o nome"
               aria-label="Full name"
-            />
+              />
+            <label
+              class="
+              block
+              uppercase
+              tracking-wide
+              text-gray-700 text-xs
+              font-bold
+              mb-2
+              "
+              for="grid-first-name"
+              >
+              Email:
+            </label>
+            <input
+              class="
+              appearance-none
+              bg-transparent
+              border-none
+              w-full
+              text-gray-700
+              text-xl
+              hover:bg-gray-200
+              rounded-full
+              mr-3
+              py-1
+              px-2
+              mb-2
+              mt-2
+              leading-tight
+              focus:outline-none
+              "
+              type="text"
+              placeholder="Informe o email"
+              aria-label="Full name"
+              />
+
           </div>
           <div class="flex flex-wrap -mx-3 mb-6"></div>
           <div class="flex flex-wrap -mx-3 mb-2">
@@ -46,58 +84,58 @@
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div
             class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8"
-          >
+            >
             <div
               class="
-                shadow
-                overflow-hidden
-                border-b border-gray-200
-                sm:rounded-lg
+              shadow
+              overflow-hidden
+              border-b border-gray-200
+              sm:rounded-lg
               "
-            >
+              >
               <table class="min-w-full divide-y divide-gray-200">
                 <thead class="bg-gray-50">
                   <tr>
                     <th
                       scope="col"
                       class="
-                        px-6
-                        py-3
-                        text-left text-xs
-                        font-medium
-                        text-gray-500
-                        uppercase
-                        tracking-wider
+                      px-6
+                      py-3
+                      text-left text-xs
+                      font-medium
+                      text-gray-500
+                      uppercase
+                      tracking-wider
                       "
-                    >
+                      >
                       Nome
                     </th>
                     <th
                       scope="col"
                       class="
-                        px-6
-                        py-3
-                        text-left text-xs
-                        font-medium
-                        text-gray-500
-                        uppercase
-                        tracking-wider
+                      px-6
+                      py-3
+                      text-left text-xs
+                      font-medium
+                      text-gray-500
+                      uppercase
+                      tracking-wider
                       "
-                    >
+                      >
                       Código
                     </th>
                     <th
                       scope="col"
                       class="
-                        px-6
-                        py-3
-                        text-left text-xs
-                        font-medium
-                        text-gray-500
-                        uppercase
-                        tracking-wider
+                      px-6
+                      py-3
+                      text-left text-xs
+                      font-medium
+                      text-gray-500
+                      uppercase
+                      tracking-wider
                       "
-                    >
+                      >
                       Caminho
                     </th>
                   </tr>
@@ -111,7 +149,7 @@
                             class="h-10 w-10 rounded-full"
                             src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
                             alt=""
-                          />
+                            />
                         </div>
                         <div class="ml-4">
                           <div class="text-sm font-medium text-gray-900">
@@ -129,16 +167,16 @@
                     <td class="px-6 py-4 whitespace-nowrap">
                       <span
                         class="
-                          px-2
-                          inline-flex
-                          text-xs
-                          leading-5
-                          font-semibold
-                          rounded-full
-                          bg-green-100
-                          text-green-800
+                        px-2
+                        inline-flex
+                        text-xs
+                        leading-5
+                        font-semibold
+                        rounded-full
+                        bg-green-100
+                        text-green-800
                         "
-                      >
+                        >
                         react/doom/virtualElements
                       </span>
                     </td>
@@ -151,7 +189,7 @@
                             class="h-10 w-10 rounded-full"
                             src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
                             alt=""
-                          />
+                            />
                         </div>
                         <div class="ml-4">
                           <div class="text-sm font-medium text-gray-900">
@@ -169,16 +207,16 @@
                     <td class="px-6 py-4 whitespace-nowrap">
                       <span
                         class="
-                          px-2
-                          inline-flex
-                          text-xs
-                          leading-5
-                          font-semibold
-                          rounded-full
-                          bg-green-100
-                          text-green-800
+                        px-2
+                        inline-flex
+                        text-xs
+                        leading-5
+                        font-semibold
+                        rounded-full
+                        bg-green-100
+                        text-green-800
                         "
-                      >
+                        >
                         react/doom/virtualElements
                       </span>
                     </td>
