f391cec3ed374348357a742952629725b8bf5abb
freaklucas <lucas.oliveira.freak@gmail.com>
2021-08-19 14:37:27 -0300

    adicionando fields

--- frontend/src/components/admin/UserAdmin.vue
+++ frontend/src/components/admin/UserAdmin.vue
@@ -348,10 +348,22 @@
 
 <script>
 export default {
-  name: "UserAdmin",
+  name: "UserAdmin",  
   components: {},
   data: function () {
-    return {};
+    return {
+      mode: 'save',
+      user: {},
+      users: [],
+      fields: [
+        { key: 'id', label: 'Código', sortable: true},
+        { key: 'name', label: 'Nome', sortable: true},
+        { key: 'email', label: 'E-mail', sortable: true},
+        { key: 'admin', label: 'Administrador', sortable: true,
+          formatter: value => value ? 'Sim' : 'Não'},
+        { key: 'actions', label: 'Ações'}
+      ]
+    };
   },
   methods: {
     loadUsers() {
