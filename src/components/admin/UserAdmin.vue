57d6f74889abd247e695f1b2c7608e087564cf4e
freaklucas <lucas.oliveira.freak@gmail.com>
2021-09-13 16:30:33 -0300

    consertando reset

--- src/components/admin/UserAdmin.vue
+++ src/components/admin/UserAdmin.vue
@@ -130,7 +130,6 @@
               focus:outline-none
               focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
             "
-            @click="reset"
           >
             Cancelar
           </button>
