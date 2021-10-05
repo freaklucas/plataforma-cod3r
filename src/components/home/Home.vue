632286215996a0867b2bd26d276bede5295e1376
freaklucas <lucas.oliveira.freak@gmail.com>
2021-10-05 18:39:38 -0300

    add redirects

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,52 +1,68 @@
 <template>
-    <div class="home">
-        <page-title icon="fa fa-home"  main="Dashboard" 
-            sub="Base do Conhecimento"
-            class="font-mono text-4xl mb-14 mt-14"
+  <div class="home">
+    <page-title
+      icon="fa fa-home"
+      main="Dashboard"
+      sub="Base do Conhecimento"
+      class="font-mono text-4xl mb-14 mt-14"
+    />
+    <div class="stats flex justify-between flex-wrap">
+      <router-link to="/admin">
+        <Stat
+          title="Categorias"
+          :value="stat.categories"
+          icon="fa fa-folder"
+          color="#d54d50"
+          class="font-bold text-base hover:bg-blue-100"
         />
-        <div class="stats flex justify-between flex-wrap">
-            <Stat title="Categorias" :value="stat.categories"
-                icon="fa fa-folder" color="#d54d50" class="font-bold text-base hover:bg-blue-100"
-            />
-            <Stat title="Artigos" :value="stat.articles"
-                icon="fa fa-file" color="#3bc480"  class="font-bold text-base hover:bg-blue-100"
-            />
-            <Stat title="Usuarios" :value="stat.users"
-                icon="fa fa-user" color="#3282cd"  class="font-bold text-base hover:bg-blue-100"
-            />
-        </div>
+      </router-link>
+      <router-link to="/articles">
+        <Stat
+          title="Artigos"
+          :value="stat.articles"
+          icon="fa fa-file"
+          color="#3bc480"
+          class="font-bold text-base hover:bg-blue-100"
+        />
+      </router-link>
+      <Stat
+        title="Usuarios"
+        :value="stat.users"
+        icon="fa fa-user"
+        color="#3282cd"
+        class="font-bold text-base hover:bg-blue-100"
+      />
     </div>
+  </div>
 </template>
 
 <script>
-
-import PageTitle from '../template/PageTitle.vue'
-import Stat from './Stat.vue'
-import axios from 'axios'
-import { baseApiUrl } from '@/global'
+import PageTitle from "../template/PageTitle.vue";
+import Stat from "./Stat.vue";
+import axios from "axios";
+import { baseApiUrl } from "@/global";
 
 export default {
-    name: 'Home',
-    components: {
-        PageTitle,
-        Stat
+  name: "Home",
+  components: {
+    PageTitle,
+    Stat,
+  },
+  data: function () {
+    return {
+      stat: {},
+    };
+  },
+  methods: {
+    getStats() {
+      axios.get(`${baseApiUrl}/stats`).then((res) => (this.stat = res.data));
     },
-    data: function () {
-        return {
-            stat: {}
-        }
-    },
-    methods: {
-        getStats() {
-            axios.get(`${baseApiUrl}/stats`).then(res => this.stat = res.data)
-        }
-    },
-    mounted() {
-        this.getStats()
-    }
-}
+  },
+  mounted() {
+    this.getStats();
+  },
+};
 </script>
 
 <style>
-
 </style>
