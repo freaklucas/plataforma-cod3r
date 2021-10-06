f6bfde91bf2a7f952e52e216a8b485bfade316c6
freaklucas <lucas.oliveira.freak@gmail.com>
2021-10-06 20:07:57 -0300

    centralizando login

--- src/components/home/Login.vue
+++ src/components/home/Login.vue
@@ -4,23 +4,10 @@
       <div class="flex h-screen">
         <div class="m-auto">
           <div class="p-8 shadow-lg rounded-xl text-center bg-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="inline text-cyan-600 h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
-              />
-            </svg>
-            <h1 class="text-3xl font-bold text-cyan-500">Login</h1>
+            <img src="../../assets/images/cod3r.png" class="inline text-cyan-600 h-24 w-24 " alt="logo da cod3r plataforma">
+            <h1 class="text-4xl font-bold text-gray-700 mt-2 mb-2">Login</h1>
             <h1 class="text-2xl font-semibold text-gray-500">
-              Faça login para aprender programação na prática
+              Faça login para aprender programação na prática!
             </h1>
             <div class="text-left pt-3">
               <input
@@ -28,15 +15,59 @@
                 placeholder="E-mail"
                 class="
                   p-1
+                  h-full
+                  w-1/2
                   rounded-lg
                   bg-gray-100
                   shadow-md
                   focus:outline-none
                   focus:border-2
                   border-cyan-500
+                  hover:bg-blue-100
                 "
               />
+              <input
+                type="password"
+                placeholder="Senha"
+                class="
+                  block
+                  p-1
+                  w-1/2
+                  mt-3
+                  rounded-lg
+                  bg-gray-100
+                  shadow-md
+                  focus:outline-none
+                  focus:border-2
+                  border-cyan-500
+                  hover:bg-blue-100
+                "
+              />
+              <a class="text-sm text-blue-600 underline-none"
+                >Esqueceu sua senha?</a
+              >
             </div>
+            <button
+              type="submit"
+              class="
+                bg-cyan-200
+                p-2
+                pl-2
+                pr-5
+                text-gray-700
+                text-center
+                font-bold
+                rounded-xl
+                border-cyan-700
+                focus:ring-2
+                m-4
+                bg-gray-300
+                hover:bg-blue-100
+                
+              "
+            >
+              Entrar
+            </button>
           </div>
         </div>
       </div>
@@ -49,4 +80,7 @@ export default {};
 </script>
 
 <style>
+  body {
+  background-color: #c9f5f9;
+}
 </style>
