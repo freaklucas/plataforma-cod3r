0e558352bf7dbb9ed2546eb1fa0914339d50bee3
freaklucas <lucas.oliveira.freak@gmail.com>
2021-10-07 00:29:38 -0300

    frontend ok

--- src/components/home/Login.vue
+++ src/components/home/Login.vue
@@ -4,17 +4,23 @@
       <div class="flex h-screen">
         <div class="m-auto">
           <div class="p-8 shadow-lg rounded-xl text-center bg-white">
-            <img src="../../assets/images/cod3r.png" class="inline text-cyan-600 h-24 w-24 " alt="logo da cod3r plataforma">
-            <h1 class="text-4xl font-bold text-gray-700 mt-2 mb-2">Login</h1>
-            <h1 class="text-2xl font-semibold text-gray-500">
-              Faça login para aprender programação na prática!
+            <img
+              src="../../assets/images/cod3r.png"
+              class="inline text-cyan-600 h-24 w-24"
+              alt="logo da cod3r plataforma"
+            />
+            <h1 class="text-4xl font-bold text-gray-700 mt-4 mb-4">Login</h1>
+            <h1 class="text-1xl font-semibold text-gray-500">
+              Faça login para aprender programação na prática
+              <img src="../../assets/icons/ideia.svg" alt="icone de ideia" class="inline w-4 h-8 mb-1">
             </h1>
-            <div class="text-left pt-3">
+            <div class="text-left pt-3 justify-center">
               <input
                 type="email"
                 placeholder="E-mail"
                 class="
                   p-1
+                  ml-20
                   h-full
                   w-1/2
                   rounded-lg
@@ -32,8 +38,10 @@
                 class="
                   block
                   p-1
+                  ml-20
                   w-1/2
                   mt-3
+                  mb-2
                   rounded-lg
                   bg-gray-100
                   shadow-md
@@ -43,7 +51,7 @@
                   hover:bg-blue-100
                 "
               />
-              <a class="text-sm text-blue-600 underline-none"
+              <a class="text-sm text-red-600 underline-none"
                 >Esqueceu sua senha?</a
               >
             </div>
@@ -54,8 +62,7 @@
                 p-2
                 pl-2
                 pr-5
-                text-gray-700
-                text-center
+                text-gray-700 text-center
                 font-bold
                 rounded-xl
                 border-cyan-700
@@ -63,7 +70,6 @@
                 m-4
                 bg-gray-300
                 hover:bg-blue-100
-                
               "
             >
               Entrar
@@ -80,7 +86,7 @@ export default {};
 </script>
 
 <style>
-  body {
+body {
   background-color: #c9f5f9;
 }
 </style>
